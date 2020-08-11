f959af3d483bfe4038046ba2998347a082675ef4
Altnar <wrk.Altnar@gmail.com>
2020-08-11 09:41:12 +0500

    add set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='testpiepnv',
+   version='1.0',
+   description='just try to make pipenv work',
+   author='ME!',
+   author_email='ME!@foo.com',
+   packages=['some_things'],  #same as name
+)
