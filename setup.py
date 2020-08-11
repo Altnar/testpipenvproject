5510135e836b0bf9e038a68fab22ce6219703915
Altnar <wrk.Altnar@gmail.com>
2020-08-11 08:35:54 +0500

    add setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
 setup(
-   name='testpipenvproject',
+   name='testpiepnv',
    version='1.0',
    description='just try to make pipenv work',
    author='ME!',
    author_email='ME!@foo.com',
-   packages=['testpipenvproject'],  #same as name
-   install_requires=['pandas'], #external packages as dependencies
+   packages=['some_things'],  #same as name
 )
